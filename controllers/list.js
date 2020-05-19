5089b825e04ce9dfa4f0eeffa2f1522c88f663d7
gjones81 <jones.guy81@gmail.com>
2020-05-19 15:03:04 -0700

    Updating List controller (PUT/DELETE)

--- controllers/list.js
+++ controllers/list.js
@@ -28,6 +28,19 @@ router.post('/', (req, res) => {
 	})
 })
 
+//PUT route to edit the start of a list
+router.put('/:id', (req, res) => {
+	db.List.findOneAndUpdate({ _id: req.params.id },{
+		listTitle: req.body.listTitle
+	})
+	.then(() => {
+		res.send({message: 'Successfully Edited a List', status: '200'})
+	})
+	.catch(err => {
+		console.log('Error', err)
+	})
+})
+
 //POST route to add items to a created list
 router.post('/item', (req, res) => {
 	db.List.findOneAndUpdate({
@@ -50,35 +63,42 @@ router.post('/item', (req, res) => {
 	})
 })
 
-//PUT route to edit items
-// router.put('/:id', (req, res) => {
-// 	db.List.findOneAndUpdate({ _id: req.params.id },{
-// 		user: req.body.user,
-// 		address: req.body.address,
-// 		item: {
-// 			name: req.body.item,
-// 			price: req.body.price,
-// 			image: req.body.image,
-// 			condition: req.body.condition
-// }
-// })
-// 	.then(() => {
-// 		res.send({ message: "Successfully Updated List", status:'200'})
-// 	})
-// 	.catch(err => {
-// 		console.log('Error', err)
-// 	})
-// })
+//PUT route to edit an item on the list
+//req.params.id NEEDS the item _id
+//req.body.listId NEEDS the list _id
+router.put('/item/:id', (req, res) => {
+	db.List.findOne({ _id: req.body.listId })
+	.then((list) => {
+		let item = list.item.id(req.params.id)
+		item.name = req.body.name
+		item.price = req.body.price
+		item.image = req.body.image
+		item.condition = req.body.condition
+		list.save()
+		.then(() =>{
+			res.send({ message: "Successfully Updated An Item", status:'200'})
+		})
+		
+	})
+	.catch(err => {
+		console.log('Error', err)
+	})
+})
 
-// //DELETE route to delete items
-// router.delete('/:id', (req, res) => {
-// 	db.List.findOneAndDelete({ _id: req.params.id})
-// 	.then(() => {
-// 		res.send({ message: "Successfully Deleted Item", status:"200"})
-// 	})
-// 	.catch(err => {
-// 		console.log('Error', err)
-// 	})
-// })
+//DELETE route to delete items
+router.delete('/item/:id', (req, res) => {
+	db.List.findOne({ _id: req.params.listId })
+	.then((list) => {
+		let item = list.item.id(req.params.id)
+		item.pull()
+		list.save()
+	})
+	.then(() => {
+		res.send({ message: "Successfully Deleted Item", status:"200"})
+	})
+	.catch(err => {
+		console.log('Error', err)
+	})
+})
 
 module.exports = router
